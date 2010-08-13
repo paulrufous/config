51bf39f0adf015fb366058a4483cb37cb2b1256c
SIO <sio.wtf@gmail.com>
2010-08-14 00:48:26 +0400

    Sat Aug 14 00:48:23 MSD 2010

--- bin/cml.sh
+++ bin/cml.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 amixer sset Master mute
+(sleep 3; pgrep xlock && xset dpms force off)&
 xlock -mode pacman -erasemode no_fade -timeelapsed -usefirst -lockdelay 3\
  -timeout 4 -font "-*-terminus-medium-r-normal-*-32-*-*-*-*-*-*-*"\
  -planfont "-*-terminus-medium-r-normal-*-32-*-*-*-*-*-*-*"\
