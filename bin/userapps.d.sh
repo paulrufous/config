785e0ce36d0c6f0c1637b81890eac4f9cd1a2256
SIO <sio.wtf@gmail.com>
2010-10-21 02:51:03 +0400

    Thu Oct 21 02:50:59 MSD 2010

--- bin/userapps.d.sh
+++ bin/userapps.d.sh
@@ -4,17 +4,23 @@
 # Запущенные коньки при незапущенных иксах выглядят жестоко, пусть и
 # только в списке процессов
 
-xxkb_daemon() {
-    xxkb; xxkb_daemon
-}
-xxkb_daemon&
+#~ ПЕРВАЯ ВЕРСИЯ. ИНДУСОКОД
+#~ xxkb_daemon() {
+    #~ xxkb; xxkb_daemon
+#~ }
+#~ xxkb_daemon&
+#~ 
+#~ 
+#~ conkympd_daemon() {
+    #~ conky; conkympd_daemon
+#~ }
+#~ conkympd_daemon&
+#~ 
+#~ conkyclock_daemon() {
+    #~ conky -c ~/.conkyrc-diclock; conkyclock_daemon
+#~ }
+#~ conkyclock_daemon&
 
-conkympd_daemon() {
-    conky; conkympd_daemon
-}
-conkympd_daemon&
-
-conkyclock_daemon() {
-    conky -c ~/.conkyrc-diclock; conkyclock_daemon
-}
-conkyclock_daemon&
+(while true; do xxkb; sleep 5; done)&
+(while true; do conky; sleep 5; done)&
+(while true; do conky -c ~/.conkyrc-diclock; sleep 5; done)&
