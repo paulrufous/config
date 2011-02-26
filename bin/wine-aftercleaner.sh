30db7f8ff1448deaacf4854092b85cdd5f2fb3b5
SIO <sio.wtf@gmail.com>
2011-02-27 00:50:13 +0300

    Sun Feb 27 00:50:11 MSK 2011

---
+++ bin/wine-aftercleaner.sh
@@ -0,0 +1,19 @@
+# 1. ВЫМАРЫВАНИЕ СОДЕРЖИМОГО
+DIRS_TO_CLEAN="$HOME/.local/share/applications $HOME/.local/share/mime"
+# Из этих папок будет удалена любая строка, содержащая слово "wine"
+
+for DIR in $DIRS_TO_CLEAN
+do
+    {
+        find "$DIR" -type f | while read FILENAME
+            do
+                grep -wi wine $FILENAME &&\
+                grep -wvi wine $FILENAME > $FILENAME
+            done
+    }
+done
+
+# 2. ПОЛНОЕ УДАЛЕНИЕ НЕКОТОРЫХ ФАЙЛОВ
+#
+# Удалить лишние иконки, *.desktop файлы и прочее
+# Дописать, когда будет на чем тестировать :)
