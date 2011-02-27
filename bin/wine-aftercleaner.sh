9623ccaf45eb04c78b9dd652d5df451e1855fd9c
SIO <sio.wtf@gmail.com>
2011-02-27 04:28:48 +0300

    Sun Feb 27 04:28:46 MSK 2011

--- bin/wine-aftercleaner.sh
+++ bin/wine-aftercleaner.sh
@@ -5,11 +5,10 @@ DIRS_TO_CLEAN="$HOME/.local/share/applications $HOME/.local/share/mime"
 for DIR in $DIRS_TO_CLEAN
 do
     {
-        find "$DIR" -type f | while read FILENAME
-            do
-                grep -wi wine "$FILENAME" &&\
-                grep -wvi wine "$FILENAME" > "$FILENAME"
-            done
+    grep -Rwi -Hl wine "$DIR" | while read FILENAME #thx to ixti@laptop
+    do
+        grep -wvi wine "$FILENAME" > "$FILENAME"
+    done
     }
 done
 
