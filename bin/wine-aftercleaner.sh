5667b1fb01661975e4948bbdda2e95111ac0b85b
SIO <sio.wtf@gmail.com>
2011-02-27 04:50:50 +0300

    Sun Feb 27 04:50:50 MSK 2011

--- bin/wine-aftercleaner.sh
+++ bin/wine-aftercleaner.sh
@@ -18,3 +18,6 @@ done
 #
 # Удалить лишние иконки, *.desktop файлы и прочее
 # Дописать, когда будет на чем тестировать :)
+rm $HOME/.local/share/applications/wine-extension-*.desktop
+rm $HOME/.local/share/mime/packages/x-wine-extension-*.xml
+rm $HOME/.local/share/mime/application/x-wine-extension-*.xml
